99167b6e69cea90b082cf367802bf6646f1dc6f6
andimon <andrevella2001@gmail.com>
2026-02-16 22:00:34 +0100

    feat: initialise project

---
+++ lib/exor.ex
@@ -0,0 +1,18 @@
+defmodule Exor do
+  @moduledoc """
+  Documentation for `Exor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
